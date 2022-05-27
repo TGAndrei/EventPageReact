bc350405a3c30fe17c4f44ef746e0fe9ee36075d
Andrei Togan <togandrei@gmail.com>
2022-05-27 20:40:44 +0300

    [React initial setup + navbar created]

---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,42 @@
+import React from "react";
+import {
+  SidebarContainer,
+  Icon,
+  CloseIcon,
+  SidebarWrapper,
+  SidebarMenu,
+  SidebarLink,
+  SidebarRoute,
+  SideBtnWrap,
+} from "./SidebarElements";
+
+const Sidebar = ({ isOpen, toggle }) => {
+  return (
+    <SidebarContainer isOpen={isOpen} onClick={toggle}>
+      <Icon onClick={toggle}>
+        <CloseIcon />
+      </Icon>
+      <SidebarWrapper>
+        <SidebarMenu>
+          <SidebarLink to="about" onClick={toggle}>
+            About
+          </SidebarLink>
+          <SidebarLink to="prices" onClick={toggle}>
+            Prices
+          </SidebarLink>
+          <SidebarLink to="artists" onClick={toggle}>
+            Artists
+          </SidebarLink>
+          <SidebarLink to="signup" onClick={toggle}>
+            Sign Up
+          </SidebarLink>
+        </SidebarMenu>
+        <SideBtnWrap>
+          <SidebarRoute to="/signin">SignIn</SidebarRoute>
+        </SideBtnWrap>
+      </SidebarWrapper>
+    </SidebarContainer>
+  );
+};
+
+export default Sidebar;
