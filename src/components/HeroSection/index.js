da7de29604f80bb8ed576d97216d675a1d13b32e
Andrei Togan <togandrei@gmail.com>
2022-05-29 17:48:47 +0300

    [Finish the Homepage]

---
+++ src/components/HeroSection/index.js
@@ -0,0 +1,47 @@
+import { hover } from "@testing-library/user-event/dist/hover";
+import React, { useState } from "react";
+import Video from "../../videos/video.mp4";
+import { Button } from "../ButtonElement";
+import {
+  HeroContainer,
+  HeroBg,
+  VideoBg,
+  HeroContent,
+  HeroH1,
+  HeroP,
+  BtnHeroWrapper,
+  ArrowForward,
+  ArrowRight,
+} from "./HeroElements";
+
+const HeroSection = () => {
+  const [hover, setHover] = useState(false);
+  const onHover = () => {
+    setHover(!hover);
+  };
+
+  return (
+    <HeroContainer>
+      <HeroBg>
+        <VideoBg autoPlay loop muted src={Video} type="video/mp4" />
+      </HeroBg>
+      <HeroContent>
+        <HeroP> Are you ready for </HeroP>
+        <HeroH1> zGeneration Festival?</HeroH1>
+        <BtnHeroWrapper>
+          <Button
+            to="signup"
+            onMouseEnter={onHover}
+            onMouseLeave={onHover}
+            primary="true"
+            dark="true"
+          >
+            Get Started {hover ? <ArrowForward /> : <ArrowRight />}
+          </Button>
+        </BtnHeroWrapper>
+      </HeroContent>
+    </HeroContainer>
+  );
+};
+
+export default HeroSection;
